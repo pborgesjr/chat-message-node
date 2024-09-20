202d94785aa928f31e5414a057c44b611ae4e022
Pedro Borges <ppmbjr@gmail.com>
2024-09-20 00:26:38 -0300

    feat: added image upload to S3 and new upload endpoint

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const express = require("express");
-const { createServer } = require("node:http");
-const cors = require("cors");
-const { Server } = require("socket.io");
-const { MongoClient, ObjectId } = require("mongodb");
+import express from "express";
+import path from "path";
+import crypto from "crypto";
+import dotenv from "dotenv";
+import multer from "multer";
+import cors from "cors";
+
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import { MongoClient, ObjectId } from "mongodb";
+import AWS from "aws-sdk";
+
+dotenv.config();
+
+export const generateFileName = (originalname) => {
+  const ext = path.extname(originalname);
+  return `${crypto.randomBytes(16).toString("hex")}${ext}`;
+};
 
 const client = new MongoClient(
   "mongodb+srv://ppmbjr:12KSV3cCxdvBBV5S@cluster0.f4pus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -12,6 +25,26 @@ const PORT = 3000;
 
 const app = express();
 app.use(express.json());
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const uploadToS3 = (buffer, filename, mimetype) => {
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: filename,
+    Body: buffer,
+    ContentType: mimetype,
+    ACL: "public-read",
+  };
+
+  return s3.upload(params).promise();
+};
 
 const server = createServer(app);
 
@@ -61,6 +94,41 @@ app.get("/conversations", async (request, response) => {
   } catch (e) {
     // Handle any errors and send a 500 response with the error message
     response.status(500).send({ message: e.message });
+  }
+});
+
+app.post("/upload", upload.single("image"), async (req, res) => {
+  const { documentID, userID } = req.body;
+  const { buffer, mimetype, originalname } = req.file;
+
+  const filename = generateFileName(originalname);
+
+  try {
+    const s3Response = await uploadToS3(buffer, filename, mimetype);
+    const imageUrl = s3Response.Location; // Get the S3 URL of the image
+
+    // Create a new message object with the image URL
+    const newMessage = {
+      _id: new ObjectId(),
+      createdAt: new Date(),
+      text: "",
+      user: {
+        _id: userID,
+      },
+      image: imageUrl, // Store the S3 image URL in the message
+    };
+
+    // Update the "chats" collection in MongoDB, pushing the new message into the messages array
+    await collection.updateOne(
+      { _id: new ObjectId(documentID) }, // Find the document by _id
+      { $push: { messages: newMessage } } // Push the new message into the messages array
+    );
+
+    // Respond with success and the image URL
+    res.status(200).json({ success: true, message: newMessage });
+  } catch (error) {
+    console.error("Error uploading to S3:", error);
+    res.status(500).send(error);
   }
 });
 
@@ -131,18 +199,21 @@ io.on("connection", (socket) => {
   });
 
   // Message event listener
-  socket.on("message", ({ message, documentID }) => {
+  socket.on("message", ({ message, documentID, avoidInsertion }) => {
     // Push the new message to the database for the active room
     console.log(`Message: ${message.text} sent on room: ${socket.activeRoom}`);
 
-    collection.updateOne(
-      { _id: new ObjectId(documentID) },
-      {
-        $push: {
-          messages: message,
-        },
-      }
-    );
+    if (!avoidInsertion) {
+      collection.updateOne(
+        { _id: new ObjectId(documentID) },
+        {
+          $push: {
+            messages: message,
+          },
+        }
+      );
+    }
+
     // Broadcast the new message to all users in the room
     io.to(socket.activeRoom).emit("message", message);
   });
